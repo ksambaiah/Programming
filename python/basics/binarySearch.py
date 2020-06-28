68eeb06171bfaa816c57f9c923cceea39ea8431f
Sambaiah Kilaru <ksambaiah@gmail.com>
2020-06-28 12:39:58 +0530

    Basic programs GCD and binary search

---
+++ python/basics/binarySearch.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import random
+
+# Generate random list of integers amounting 1000 
+def intArray(size):
+    step = 1
+    arr = []
+    arr.append(100)
+    for i in range(1, size):
+        arr.append(random.randint(arr[i-1], arr[i-1]+step))
+    return arr
+
+def binarySearch(arr, ele, i, j):
+    middle = int((j-i)/2) + i
+    if j >= i:
+        if arr[middle] == ele:
+           return middle
+        elif arr[middle] < ele:
+           return binarySearch(arr, ele, middle+1, j)
+        elif arr[middle] > ele:
+           return binarySearch(arr, ele, i, middle-1)
+    else:
+        return -1
+if __name__ == "__main__": 
+   size = 30000 
+   intArray(size) 
+   sortedArray = intArray(size) 
+   ele = random.randint(sortedArray[0], sortedArray[-1])
+   ele = 999
+   match = binarySearch(sortedArray, ele, 0, len(sortedArray)-1)
+   if match == -1:
+      print("Element % d is not in the array" %(ele))
+   else:
+      print("Element % d is in the list % d" %(ele, match))
+  
---
+++ python/basics/gcd.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import random
+
+# Computing GCD of two numbers, how do we get two positive integers?
+# Using Random integers
+def gcd(i, j):
+    if j == 0:
+       return i
+    else:
+       return gcd(j, i%j) 
+
+if __name__ == "__main__": 
+   a = random.randint(1, 9999999999)
+   b = random.randint(1, 99999999999999)
+   if a > b:
+      r = gcd(a, b)
+   else:
+      r = gcd(b, a)
+   print("GCD of % d and % d is % d" %(a, b, r))
